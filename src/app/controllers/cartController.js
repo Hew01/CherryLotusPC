e738336661cd43f0e2ff7dcd0e82ad5271227b90
Thanh Duy <duythanhpham2603@gmail.com>
2023-11-17 23:55:22 +0700

    Fix cart render

--- src/app/controllers/cartController.js
+++ src/app/controllers/cartController.js
@@ -4,15 +4,19 @@ const productModel = require('../models/product')
 class CartController {
     //[GET] /cart/:id
     async getCart(req, res, next) {
-        const cart = await cartModel.findOne({userId : req.params.id})
         var productsResult = []
-        cart.products.forEach(async (item) => {
-            const product = await productModel.findOne({ _id : item.productId })
-            productsResult.push({
-                ...product,
-                quantity : item.quantity,
+        const cart = await cartModel.findOne({userId : req.params.id})
+        if(cart) {
+            cart?.products.forEach(async (item) => {
+                const product = await productModel.findOne({ _id : item.productId })
+                if(product) {
+                    productsResult.push({
+                        ...product,
+                        quantity : item.quantity,
+                    })
+                }
             })
-        })
+        }
         res.render('cart', { products: productsResult })
     }
 }
