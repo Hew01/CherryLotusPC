a2ef71ec52f1e8d3d63d39b021a617c88835c7da
Nguyen Khanh Duy <21522003@gm.uit.edu.vn>
2023-11-21 05:57:31 +0700

    update cart

--- src/public/javascripts/cart.js
+++ src/public/javascripts/cart.js
@@ -1,149 +1,369 @@
-window.addEventListener("DOMContentLoaded", () => {
-  var currentStep = 1;
-  const headerCartSteps = document.querySelectorAll(".header-cart__step");
-  const orderButtonElement = document.querySelector(".order-now");
-  const forms = document.querySelectorAll("section");
-  const totalContainerElement = document.querySelector(".section-info-total");
+// window.addEventListener("DOMContentLoaded", () => {
+var currentStep = 1;
+var infoCustomer = {
+  name: "",
+  phoneNumber: "",
+  province: "",
+  district: "",
+  ward: "",
+  home: "",
+};
 
-  const updateProgress = () => {
-    headerCartSteps.forEach((step, index) => {
-      if (index + 1 <= currentStep && index != 0) {
-        step.classList.add("active");
-      } else {
-        step.classList.remove("active");
-      }
-    });
-  };
+const headerCartSteps = document.querySelectorAll(".header-cart__step");
+const orderButtonElement = document.querySelector(".order-now");
+const orderButtonElementFinal = document.querySelector(".order-now-final");
+const forms = document.querySelectorAll("section");
+const totalContainerElement = document.querySelector(".section-info-total");
 
-  const updateProductElement = () => {
-    totalContainerElement.style.display = "block";
-    forms.forEach((form, index) => {
-      if (index + 1 === currentStep) {
-        if (currentStep === 4) {
-          totalContainerElement.style.display = "none";
-        }
-        form.classList.add("active");
-      } else {
-        form.classList.remove("active");
-      }
-    });
-  };
+const nameCustomer = document.querySelector("#name-customer");
+const phoneCustomer = document.querySelector("#phone-customer");
+const provincesCustomer = document.querySelector("#provinces");
+const districtCustomer = document.querySelector("#district");
+const wardCustomer = document.querySelector("#wards");
+const adressHome = document.querySelector("#adress-home");
 
-  const handleMoveNextStep = () => {
-    if (currentStep === 4) {
-      return;
+const orderMore = document.querySelector(".breadcrumb-cart--link-product");
+const backStep = document.querySelector(".back");
+const backStepIcon = document.querySelectorAll(".header-cart__step--icon");
+const orderNow = document.querySelector(".order-now");
+const orderNowFinal = document.querySelector(".order-now-final");
+
+const updateInfo = document.querySelectorAll(".innerText-box");
+const updateFinalInfo = document.querySelectorAll(".final-info");
+
+//Hàm cập nhật trạng thái của các step ở phần header
+const updateProgress = () => {
+  headerCartSteps.forEach((step, index) => {
+    if (index + 1 <= currentStep && index != 0) {
+      step.classList.add("active");
+    } else {
+      step.classList.remove("active");
     }
-    currentStep++;
+  });
+};
+
+const updateProductElement = () => {
+  totalContainerElement.style.display = "block";
+  forms.forEach((form, index) => {
+    if (index + 1 === currentStep) {
+      if (currentStep === 4) {
+        totalContainerElement.style.display = "none";
+      }
+      form.classList.add("active");
+    } else {
+      form.classList.remove("active");
+    }
+  });
+};
+
+function nextStep() {}
+
+function updateStep() {
+  if (currentStep === 2 || currentStep === 3) {
+    backStep.classList.add("active");
+    orderMore.classList.remove("active");
+  }
+  if (currentStep === 1) {
+    backStep.classList.remove("active");
+    orderMore.classList.add("active");
+  }
+  if (currentStep === 4) {
+    backStep.classList.remove("active");
+    orderMore.classList.remove("active");
+  }
+}
+
+function updateOrderNow() {
+  if (currentStep === 1 || currentStep === 2) {
+    orderNow.classList.add("active");
+    orderNowFinal.classList.remove("active");
+  }
+  if (currentStep === 3) {
+    orderNow.classList.remove("active");
+    orderNowFinal.classList.add("active");
+  }
+  if (currentStep === 4) {
+    orderNow.classList.remove("active");
+    orderNowFinal.classList.remove("active");
+  }
+}
+
+const handleBackStep = () => {
+  currentStep--;
+  updateProgress();
+  updateProductElement();
+  updateStep();
+};
+
+const handleBackStepIcon = () => {
+  backStepIcon.forEach((step, index) => {
+    if (index < backStepIcon.length - 2) {
+      step.onclick = function () {
+        currentStep = index + 1;
+        updateProgress();
+        updateProductElement();
+        updateOrderNow();
+        updateStep();
+      };
+    }
+  });
+};
+
+handleBackStepIcon();
+
+const handleMoveNextStep = () => {
+  if (currentStep === 4) {
+    return;
+  }
+  if (currentStep === 1) {
+  }
+  if (currentStep === 2) {
+    let isEmpty = checkEmptyInvalid([
+      phoneCustomer,
+      nameCustomer,
+      provincesCustomer,
+      districtCustomer,
+      wardCustomer,
+      adressHome,
+    ]);
+    let isLength = checkLengthPhone(phoneCustomer);
+    if (isEmpty) {
+      return;
+    } else {
+      if (isLength) {
+        return;
+      }
+      updateProgress();
+      updateProductElement();
+      updateUserInfo();
+      updateInfoCustomerPayment();
+    }
+  }
+  if (currentStep === 3) {
     updateProgress();
     updateProductElement();
-    console.log(currentStep);
-  };
-
+    updateInfoCustomerPaymentFinal();
+  }
+  currentStep++;
   updateProgress();
+  updateProductElement();
+  updateStep();
+  updateOrderNow();
+};
 
-  // Start Select Tỉnh, TP
+updateProgress();
+updateStep();
+updateOrderNow();
+
+// set events
+orderButtonElement.onclick = handleMoveNextStep;
+orderButtonElementFinal.onclick = handleMoveNextStep;
+backStep.onclick = handleBackStep;
+
+function updateUserInfo() {
+  // get value from input
+  infoCustomer.name = nameCustomer.value;
+  infoCustomer.phoneNumber = phoneCustomer.value;
+  // let getProvince = provinceCustomer.value;
+  provincesCustomer.querySelectorAll("option").forEach((option) => {
+    if (option.selected) {
+      infoCustomer.province = option.innerText;
+    }
+  });
+  districtCustomer.querySelectorAll("option").forEach((option) => {
+    if (option.selected) {
+      infoCustomer.district = option.innerText;
+    }
+  });
+  wardCustomer.querySelectorAll("option").forEach((option) => {
+    if (option.selected) {
+      infoCustomer.ward = option.innerText;
+    }
+  });
+  infoCustomer.home = adressHome.value;
+}
+var adressCustomer = {
+  nameCus: "",
+  phoneCus: "",
+  adressCus: "",
+};
+
+function updateInfoCustomerPayment() {
+  adressCustomer.adressCus =
+    infoCustomer.home +
+    "," +
+    infoCustomer.ward +
+    "," +
+    infoCustomer.district +
+    "," +
+    infoCustomer.province;
+  adressCustomer.nameCus = infoCustomer.name;
+  adressCustomer.phoneCus = infoCustomer.phoneNumber;
+  updateInfo.forEach((input, index) => {
+    if (index === 0) {
+      input.innerText = adressCustomer.nameCus;
+    }
+    if (index === 1) {
+      input.innerText = adressCustomer.phoneCus;
+    }
+    if (index === 2) {
+      input.innerText = adressCustomer.adressCus;
+    }
+  });
+}
+
+function updateInfoCustomerPaymentFinal() {
+  adressCustomer.adressCus =
+    infoCustomer.home +
+    "," +
+    infoCustomer.ward +
+    "," +
+    infoCustomer.district +
+    "," +
+    infoCustomer.province;
+  adressCustomer.nameCus = infoCustomer.name;
+  adressCustomer.phoneCus = infoCustomer.phoneNumber;
+  updateFinalInfo.forEach((input, index) => {
+    if (index === 0) {
+      input.innerText = adressCustomer.nameCus;
+    }
+    if (index === 1) {
+      input.innerText = adressCustomer.phoneCus;
+    }
+    if (index === 2) {
+      input.innerText = adressCustomer.adressCus;
+    }
+  });
+}
+
+// Validate form
+
+function showError(input) {
+  let parent = input.parentElement;
+  parent.classList.add("error");
+}
+
+function showSuccess(input) {
+  let parent = input.parentElement;
+  parent.classList.remove("error");
+}
+
+function checkLengthPhone(input) {
+  const value = input.value;
+  let isLength10 = false;
+  if (value.length < 10 || value.length > 12 || value[0] !== "0") {
+    showError(input);
+    isLength10 = true;
+  } else {
+    showSuccess(input);
+  }
+  return isLength10;
+}
+
+function checkEmptyInvalid(listInput) {
+  let isEmptyError = false;
+  listInput.forEach((input) => {
+    input.value = input.value.trim();
+
+    if (!input.value) {
+      showError(input);
+      isEmptyError = true;
+      if (
+        (input.onclick = function () {
+          showSuccess(input);
+        })
+      );
+    } else {
+      showSuccess(input);
+    }
+  });
+  return isEmptyError;
+}
+
+// Start Select Tỉnh, TP
+fetch(
+  `https://vnprovinces.pythonanywhere.com/api/provinces/?basic=true&limit=100`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    let provinces = data.results;
+    provinces.map(
+      (value) =>
+        (document.getElementById(
+          "provinces"
+        ).innerHTML += `<option value='${value.id}'>${value.name}</option>`)
+    );
+  })
+
+  .catch((error) => {
+    console.error("Lỗi khi gọi api", error);
+  });
+
+// Select huyện
+
+function fetchDistricts(provincesID) {
   fetch(
-    `https://vnprovinces.pythonanywhere.com/api/provinces/?basic=true&limit=100`
+    `https://vnprovinces.pythonanywhere.com/api/districts/?province_id=${provincesID}&basic=true&limit=100`
   )
     .then((response) => response.json())
     .then((data) => {
-      let provinces = data.results;
-      provinces.map(
-        (value) =>
-          (document.getElementById(
-            "provinces"
-          ).innerHTML += `<option value='${value.id}'>${value.name}</option>`)
-      );
+      let district = data.results;
+      let count_object = district.length;
+      document.getElementById(
+        "district"
+      ).innerHTML = `<option value=''>Chọn Quận, Huyện</option>`;
+      if (count_object < 100) {
+        district.map(
+          (value) =>
+            (document.getElementById(
+              "district"
+            ).innerHTML += `<option value="${value.id}">${value.name}</option>`)
+        );
+      }
     })
-
     .catch((error) => {
       console.error("Lỗi khi gọi api", error);
     });
+}
+//Select xax phuong
+function fetchWards(districtsID) {
+  fetch(
+    `https://vnprovinces.pythonanywhere.com/api/wards/?district_id=${districtsID}&basic=true&limit=100`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let wards = data.results;
+      let count_object = wards.length;
+      document.getElementById(
+        "wards"
+      ).innerHTML = `<option value=''>Chọn Phường, Xã</option>`;
+      if (count_object < 90) {
+        wards.map(
+          (value) =>
+            (document.getElementById(
+              "wards"
+            ).innerHTML += `<option value="${value.id}">${value.name}</option>`)
+        );
+      }
+    })
+    .catch((error) => {
+      console.error("Lỗi khi gọi api", error);
+    });
+}
+// Đang sử dụng biến toàn cục window.event nên không thể set onchange với tham số event
+//  =>> Loại bỏ biến event
+function getProvinces() {
+  // Biến kiểm tra
+  var event = window.event || event;
+  fetchDistricts(event.target.value);
+  document.getElementById(
+    "wards"
+  ).innerHTML = `<option value="">Chọn Phường, Xã</option>`;
+}
 
-  // Select huyện
-
-  function fetchDistricts(provincesID) {
-    fetch(
-      `https://vnprovinces.pythonanywhere.com/api/districts/?province_id=${provincesID}&basic=true&limit=100`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        let district = data.results;
-        let count_object = district.length;
-        document.getElementById(
-          "district"
-        ).innerHTML = `<option value=''>Chọn Quận, Huyện</option>`;
-        if (count_object < 100) {
-          district.map(
-            (value) =>
-              (document.getElementById(
-                "district"
-              ).innerHTML += `<option value="${value.id}">${value.name}</option>`)
-          );
-        }
-      })
-      .catch((error) => {
-        console.error("Lỗi khi gọi api", error);
-      });
-  }
-  //Select xax phuong
-  function fetchWards(districtsID) {
-    fetch(
-      `https://vnprovinces.pythonanywhere.com/api/wards/?district_id=${districtsID}&basic=true&limit=100`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        let wards = data.results;
-        let count_object = wards.length;
-        document.getElementById(
-          "wards"
-        ).innerHTML = `<option value=''>Chọn Quận, Huyện</option>`;
-        if (count_object < 90) {
-          wards.map(
-            (value) =>
-              (document.getElementById(
-                "wards"
-              ).innerHTML += `<option value="${value.id}">${value.name}</option>`)
-          );
-        }
-      })
-      .catch((error) => {
-        console.error("Lỗi khi gọi api", error);
-      });
-  }
-
-  // set events
-  orderButtonElement.onclick = handleMoveNextStep;
-
-  function getProvinces(event) {
-    fetchDistricts(event.target.value);
-    document.getElementById(
-      "wards"
-    ).innerHTML = `<option value=''>Chọn Quận, Huyện</option>`;
-  }
-
-  function getDistricts(event) {
-    fetchWards(event.target.value);
-  }
-  // END SELECT Tỉnh Thành Phố
-
-  // Bắt đầu nút click điền thông tin công ty nếu lấy hóa đơn
-  // Lấy tham chiếu đến checkbox
-  const checkbox = document.querySelector(".tick-input");
-
-  // Lấy tham chiếu đến phần tử .form-group
-  const formGroup = document.querySelector(".form-group");
-
-  // Thêm sự kiện 'change' cho checkbox
-  checkbox.addEventListener("change", function () {
-    // Kiểm tra trạng thái của checkbox và hiển thị/ẩn phần tử .form-group tương ứng
-    formGroup.style.display = this.checked ? "block" : "none";
-  });
-
-  // Kết thuc nút click điền thông tin công ty nếu lấy hóa đơn
-
-  // Lấy thông tin sản phẩm
-
-  // Kết thúc lấy thông tin sản phẩm
-});
+function getDistricts() {
+  // fetchWards(event.target.value);
+  var event = window.event || event;
+  fetchWards(event.target.value);
+}
+// END SELECT Tỉnh Thành Phố
