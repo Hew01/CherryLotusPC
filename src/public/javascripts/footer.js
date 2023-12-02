1ba7e4ea1ea7deb30f1e19ea217ec2e3ee2bfb4b
IllyaChanNQAnh <21520565@gm.uit.edu.vn>
2023-12-02 13:35:38 +0700

    sửa lại js footer tí

--- src/public/javascripts/footer.js
+++ src/public/javascripts/footer.js
@@ -1,37 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Lấy danh sách các tab và các phần thông tin tương ứng
-    var tabs = document.querySelectorAll(".footer__info-section h4");
-    var infoSections = document.querySelectorAll(".footer__info-section");
-    var h4Elements = document.querySelectorAll("#footer h4");
-    
-    // Ẩn tất cả các thẻ con của h4 mặc định
-    h4Elements.forEach(function (h4) {
+    function checkScreenWidth() {
+      var isSmallScreen = window.innerWidth < 760;
+  
+      var h4Elements = document.querySelectorAll("#footer h4");
+      h4Elements.forEach(function (h4) {
         var ul = h4.nextElementSibling;
-        ul.style.display = "none";
-    });
+        if (isSmallScreen) {
+          ul.style.display = "none";
+        } else {
+          ul.style.display = "block";
+        }
+      });
 
-    h4Elements.forEach(function (h4) {
-        h4.addEventListener("click", function () {
-            // Ẩn/hiện các thẻ con của h4
-            var ul = h4.nextElementSibling; // Lấy thẻ ul kế tiếp của h4
-            if (ul.style.display === "block") {
-                ul.style.display = "none";
-            } else {
-                ul.style.display = "block";
-            }
-        });
-    });
+      h4Elements.forEach(function (h4) {
+        if (isSmallScreen) {
+          h4.addEventListener("click", toggleUlDisplay);
+        } else {
+          h4.removeEventListener("click", toggleUlDisplay);
+        }
+      });
 
-    // Xử lý khi click vào tab
-    tabs.forEach(function (tab, index) {
+      var tabs = document.querySelectorAll(".footer__info-section h4 span");
+      var infoSections = document.querySelectorAll(".footer__info-section");
+  
+      tabs.forEach(function (tab, index) {
         tab.addEventListener("click", function () {
-            // Ẩn tất cả các phần thông tin
-            infoSections.forEach(function (section) {
-                section.classList.remove("active");
-            });
+          infoSections.forEach(function (section) {
+            section.classList.remove("active");
+          });
 
-            // Hiển thị phần thông tin tương ứng với tab được click
-            infoSections[index].classList.add("active");
+          infoSections[index].classList.add("active");
         });
-    });
-});
+      });
+    }
+
+    checkScreenWidth();
+
+    window.addEventListener("resize", checkScreenWidth);
+
+    function toggleUlDisplay() {
+      var ul = this.nextElementSibling;
+      if (ul.style.display === "block") {
+        ul.style.display = "none";
+      } else {
+        ul.style.display = "block";
+      }
+    }
+  });
