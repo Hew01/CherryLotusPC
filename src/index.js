45d3535d2c67511140499b2daf2d97eb66075046
Hew01 <maiphuctam62@gmail.com>
2023-12-03 21:27:08 +0700

    build: update index.js
    Added xss protection using xss Shield for Express.js
    Added https configuration with key and certification for localhost usage
    Needed reconfig from http to https

--- src/index.js
+++ src/index.js
@@ -1,40 +1,52 @@
-const express = require('express')
-const path = require('path')
-const livereLoad = require('livereload')
-const connectLivereload = require('connect-livereload')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const { connectDatabase } = require('./app/config/database')
-const route = require('./routes')
-const handlebarsHelpers = require('./app/config/helpers')
+// Import modules
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const livereload = require('livereload');
+const connectLivereload = require('connect-livereload');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const { connectDatabase } = require('./app/config/database');
+const route = require('./routes');
+const handlebarsHelpers = require('./app/config/helpers');
 
-const app = express()
+// Create an Express app
+const app = express();
 
-const liveReloadServer = livereLoad.createServer()
+// Apply middlewares
+app.use(bodyParser.json());
+app.use(express.json({ type: "*/*" }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// apply midlewares
-app.use(bodyParser.json())
-app.use(express.json({
-    type: "*/*"
-}))
-app.use(bodyParser.urlencoded({ extended : true }))
+// Set up morgan to log requests
+app.use(morgan('combined'));
 
-liveReloadServer.watch(path.join(__dirname, 'public'))
-app.use(express.static(path.join(__dirname,'public')))
-app.use(connectLivereload())
+// Set up template engine with handlebars
+app.engine('.hbs', handlebars.engine({ extname: '.hbs', helpers: handlebarsHelpers }));
+app.set('view engine', '.hbs');
+app.set('views', './src/views');
 
-// set up morgan to log requests
-app.use(morgan('combined'))
+// Connect to MongoDB
+connectDatabase(app);
 
-// set up template engine with handlebars
-app.engine('.hbs', handlebars.engine({ extname : '.hbs', helpers: handlebarsHelpers }))
-app.set('view engine', '.hbs')
-app.set('views', './src/views')
+// Routing server
+route(app);
 
-// connect to MongoDB
-connectDatabase(app)
+// Create a livereload server
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+app.use(connectLivereload());
 
-// routing server
-route(app)
+// Read the certificate and key files
+const cert = fs.readFileSync('./gearuit.com+5.pem');
+const key = fs.readFileSync('./gearuit.com+5-key.pem');
 
+// Create an HTTPS server with the cert and key
+const server = https.createServer({cert, key}, app);
+
+// Start the server
+server.listen(3000, () => {
+  console.log('Server listening on https://localhost:3000');
+});
